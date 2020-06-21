fa9a3773527a000b1db1fe4a7425ef762a08ef0c
Tae Lee <taebumlee@gmail.com>
2020-06-21 12:42:11 -0700

    more concise expression

--- sumNumbersInRange.js
+++ sumNumbersInRange.js
@@ -1,8 +1,6 @@
 function sumAll(arr) {
-  const first = Math.min.apply(Math, arr); // 둘 이상의 원소를 같는 배열에도 적용
-  const last = Math.max.apply(Math, arr);
   let result = 0;
-  for (let i = first; i <= last; i++) {
+  for (let i = Math.min.apply(Math, arr); i <= Math.max.apply(Math, arr); i++) {
     result += i;
   } 
   return result;
