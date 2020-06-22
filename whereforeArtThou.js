d80a6125a2dbf95d1aaf43c009fe4c10937ff199
Tae Lee <taebumlee@gmail.com>
2020-06-21 22:49:44 -0700

    Update whereforeArtThou.js

--- whereforeArtThou.js
+++ whereforeArtThou.js
@@ -1,13 +1,20 @@
 function whatIsInAName(collection, source) {
-  var arr = [];
-  console.log(Object.keys(source), Object.values(source));
-  console.log(Object.keys(collection), Object.values(collection));
-  return arr;
+  let srcKeys = Object.keys(source);
+
+  return collection.filter((obj) => {
+    for (let i = 0; i < srcKeys.length; i++) {
+      console.log(srcKeys[i], obj[srcKeys[i]], source[srcKeys[i]])
+      if(!obj.hasOwnProperty(srcKeys[i]) || obj[srcKeys[i]] !== source[srcKeys[i]]) {
+        return false;
+      }
+    }
+    return true;
+  });
 }
 
 console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], 
                            { last: "Capulet" }));
-
+/*
 console.log(whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], 
                            { "apple": 1 }));
 
@@ -22,3 +29,4 @@ console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 
 
 console.log(whatIsInAName([{"a": 1, "b": 2, "c": 3}], 
                            {"a": 1, "b": 9999, "c": 3}));
+*/
