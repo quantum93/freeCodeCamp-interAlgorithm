9c7b2a1da9eeb22fd43b37ae2a2e2c4bc62dc5d8
Tae Lee <taebumlee@gmail.com>
2020-06-22 09:59:03 -0700

    Update pigLatin.js

--- pigLatin.js
+++ pigLatin.js
@@ -2,6 +2,11 @@ function translatePigLatin(str) {
   let myRegex = /^[^aeiou]+/
   let myConsonant = str.match(myRegex);
   console.log(myConsonant);
+  if (myConsonant === null) {
+    return str.concat("way");
+  } else {
+    return str.split(myRegex).concat(myConsonant).concat("ay").join("");
+  }
 }
 
 console.log(translatePigLatin("california"));
