fce5cb3d558840b42aea3f4ac72a9b8fb0519216
Tae Lee <taebumlee@gmail.com>
2020-06-23 19:49:39 -0700

    Update smallestCommonMultiple.js

--- smallestCommonMultiple.js
+++ smallestCommonMultiple.js
@@ -1,10 +1,12 @@
 function smallestCommons(arr) {
   const numList = [];
-  for (let i = Math.min(...arr); i <= Math.max(...arr); i++) {
-    numList.push(i);
-  }
-  return numList;
+  for (let i = Math.min(...arr); i <= Math.max(...arr); i++) { numList.push(i); }
+  
+  let startNum = 0;
+  (numList[0] === 1) ? startNum = numList[1]: startNum = numList[0];
+  console.log(numList, startNum);
+
 }
 
 
-console.log(smallestCommons([1,5]));
+console.log(smallestCommons([5,1]));
