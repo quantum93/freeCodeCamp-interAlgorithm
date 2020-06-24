1a27be971b12440eb5f8da473fc5cf0e5ae95390
Tae Lee <taebumlee@gmail.com>
2020-06-23 17:33:43 -0700

    Update sumAllPrime.js

--- sumAllPrime.js
+++ sumAllPrime.js
@@ -1,6 +1,28 @@
 function sumPrimes(num) {
-  
+
   function test_prime(n) {
+    for (let i = 2; i < n; i++) {
+      if(n % i === 0) return false;
+    }
+    return n !== 1 && n !== 0; 
+  } 
+  
+  let primeSum = 0;
+  let arr = Array.from({length:num},(v,k)=>k+1);
+  
+  for (let i = 1; i < arr.length; i++) {
+    if(test_prime(i) === true) {
+      primeSum += i;
+    }
+  }
+  return primeSum;
+}
+
+console.log(sumPrimes(10));
+console.log(sumPrimes(977));
+
+
+/*   function test_prime(n) {
     if (n===1) { return false; }
     else if(n === 2) { return true; }
     else {
@@ -9,17 +31,4 @@ function sumPrimes(num) {
       }
       return true;  
     }
-  }
-
-  let primeSum = 0;
-    if(test_prime(num) === true) {
-      primeSum += num
-    }
-
-  return primeSum;
-}
-
-console.log(sumPrimes(7));
-// console.log(sumPrimes(977));
-
-// let arr = Array.from({length:num},(v,k)=>k+1);
+  } */
