299a09075d4a1369b8c94a7b6d133f5a2dc9444d
Tae Lee <taebumlee@gmail.com>
2020-06-23 20:52:29 -0700

    Update smallestCommonMultiple.js
    it works anyway

--- smallestCommonMultiple.js
+++ smallestCommonMultiple.js
@@ -1,11 +1,32 @@
 function smallestCommons(arr) {
-  const numList = [];
-  for (let i = Math.min(...arr); i <= Math.max(...arr); i++) { numList.push(i); }
+  const numArr = [];
+  for (let i = Math.min(...arr); i <= Math.max(...arr); i++) {
+     numArr.push(i); 
+    }
   
-  let startNum = 0;
-  (numList[0] === 1) ? startNum = numList[1]: startNum = numList[0];
-  console.log(numList, startNum);
+  let multiNum = numArr.slice(-1)[0];
 
+  let checkArr = [];
+  (numArr[0] === 1) ? 
+    checkArr = numArr.slice(1, -1): 
+    checkArr = numArr.slice(0, -1);
+  
+/*   for (let i = 2; i <= 12; i++) {
+    const multiArr = checkArr.filter(num => (multiNum * i) % num === 0);
+    if(checkArr.length === multiArr.length) {
+      return multiNum * i;
+    }
+  } */
+
+  let i = 2;
+  let multiArr = [];
+  let result = 0;
+  while (checkArr.length !== multiArr.length) {
+    multiArr = checkArr.filter(num => (multiNum * i) % num === 0);
+    result = multiNum * i;
+    i++;
+  }
+  return result;
 }
 
 
