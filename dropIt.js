fb3fdbe64c6f26d40384ad51d4cde3745816d0d0
Tae Lee <taebumlee@gmail.com>
2020-06-23 23:22:05 -0700

    Update dropIt.js

--- dropIt.js
+++ dropIt.js
@@ -4,3 +4,5 @@ function dropElements(arr, func) {
 }
 
 console.log(dropElements([1, 2, 3], function(n) {return n < 3; }));
+console.log(dropElements([0, 1, 0, 1], function(n) {return n === 1;}));
+console.log(dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}));
