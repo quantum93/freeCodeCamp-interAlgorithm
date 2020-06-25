aadc7b09bbfbf90ca7d9e33fabf074ba63b2d244
Tae Lee <taebumlee@gmail.com>
2020-06-25 00:28:47 -0700

    still working

---
+++ EverythingBeTrue.js
@@ -0,0 +1,5 @@
+function truthCheck(collection, pre) {
+  return collection.map(obj => obj);
+}
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
