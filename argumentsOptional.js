08a93c05e3c05ff8836444f8a40efb5ddbe151ca
Tae Lee <taebumlee@gmail.com>
2020-06-25 17:10:38 -0700

    Update argumentsOptional.js

--- argumentsOptional.js
+++ argumentsOptional.js
@@ -1,17 +1,33 @@
-function addTogether() {
-console.log(arguments[0]);
-/*   if(typeof(arguments[0]) === "number" && typeof(arguments[1]) === "number") {
-    return [arguments[0], arguments[1]].reduce((previous, current) => {
-    return previous + current;
-    });
-  } else {
-    return undefined;
-  } */
+function addTogether(a, b) {
+  if(arguments.length === 1 && typeof(a) === "number") {
+    return function(b) {
+      if(typeof(b) === "number") {
+        return a + b;
+      }
+    }
+  } else if (arguments.length === 2 && typeof(b) === "number") {
+    return a + b;
+  }
 }
 
-//console.log(addTogether(2, 3))
-//console.log(addTogether(23, 30))
-//console.log(addTogether(2, "3"))
-//console.log(addTogether("http://bit.ly/IqT6zt"))
+console.log(addTogether("http://bit.ly/IqT6zt"))
+console.log(addTogether(2)([3]))
 console.log(addTogether(5)(7))
-//console.log(addTogether(2)([3]))
+console.log(addTogether(2, "3"))
+console.log(addTogether(2, 3))
+console.log(addTogether(23, 30))
+
+/* function addTogether(a, b) {
+  if (typeof(a) !== "number") {
+    return undefined;
+  } else if (arguments.length === 1) {
+    return function(b) { 
+      if(typeof(b) === "number") {
+        return a + b;      
+      } else {
+        return undefined;
+      }
+    };
+  } 
+  return a + b;
+} */
